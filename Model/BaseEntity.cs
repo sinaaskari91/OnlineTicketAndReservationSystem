6496336e0a9e8bccd59ea7c28b8f83330b70e0fa
hasan askari <sinaaskari91@gmail.com>
2024-09-17 23:45:07 +0330

    Add project files.

---
+++ Model/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Entities;
+
+namespace Model
+{
+    public class BaseEntity<T> where T : struct
+    {
+        public T Id { get; set; }
+        /// <summary>
+        /// The date time of data has created in system
+        /// </summary>
+        public DateTime CreatedDateTime { get; set; }
+        /// <summary>
+        /// The date time of data has updated in system
+        /// </summary>
+        public DateTime UpdatedDateTime { get; set; }
+        public Guid? CreatedUserId { get; set; }
+        public Guid? UpdatedUserId { get; set; }
+        public User? CreatedUser { get; set; }
+        public User? UpdatedUser { get; set; }
+
+    }
+}
---
+++ Model/Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class Role:BaseEntity<Guid>
+    {
+       
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
