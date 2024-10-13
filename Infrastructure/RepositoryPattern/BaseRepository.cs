a29867ca7bda87251c88520b70ad26babfc6cf19
hasan askari <sinaaskari91@gmail.com>
2024-10-13 22:32:42 +0330

    edit method includable

--- Infrastructure/RepositoryPattern/BaseRepository.cs
+++ Infrastructure/RepositoryPattern/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Model;
 using Model.Entities;
 using System;
@@ -32,14 +33,18 @@ namespace Infrastructure.RepositoryPattern
         {
             if (predicate == null)
             {
-              if(IsNoTracking)
-               return await Task.Run(()=> _entitySet.AsQueryable().AsNoTracking());
-              return await Task.Run(() => _entitySet.AsQueryable());
-
+               return await Task.Run(()=> IsNoTracking? _entitySet.AsNoTracking(): _entitySet.AsQueryable());
             }
-            return await Task.Run(() => _entitySet.Where(predicate));
+            return await Task.Run(() => IsNoTracking ? _entitySet.Where(predicate).AsNoTracking() : _entitySet.Where(predicate));
         }
-
+        public async Task<IQueryable<TResult>> GetAllAsync<TResult>(Expression<Func<T, TResult>> selector, Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool IsNoTracking = true)
+        {
+            IQueryable<T> query=_entitySet;
+            query=IsNoTracking==true ? query.AsNoTracking() : query.AsQueryable();
+            query = predicate != null ? query.Where(predicate) : query;
+            query = include != null ? include(query) : query;
+           return  query != null && query.Any() ? query.Select(selector) : default;
+        }
 
         public async Task<T> GetAsync(Expression<Func<T, bool>> predicate)
         {
